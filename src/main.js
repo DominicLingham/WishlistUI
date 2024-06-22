c9e79accedcb1829808481244ad948dfd7dd24e1
Dominic Lingham <103593100+DominicLingham@users.noreply.github.com>
2024-06-22 18:09:16 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import '../node_modules/papercss/dist/paper.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="paper container-xs margin-bottom-large">
+        <h1>Test Login</h1>
+        <div class="column flex-center">
+            <form @submit.prevent>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input v-model="username" class="input-block" type="text" id="username" />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input v-model="password" class="input-block" type="password" id="password" />
+                </div>
+                <button @click="logPassword">Login</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const username = ref('')
+const password = ref('')
+
+const logPassword = () => {
+    const userPassword = password.value
+    const userUsername = username.value
+
+    console.log(`username: ${userUsername}, password: ${userPassword}`)
+}
+</script>
+
+<style scoped>
+h1 {
+    color: darkred;
+}
+
+label {
+    color: darkred;
+}
+
+p {
+    color: green;
+}
+</style>
